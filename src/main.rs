2f05b6d055fffd2f4df550cbe7b5cc0d7f756c5c
Hakan Özler <ozler.hakan@gmail.com>
2023-02-12 14:09:32 +0300

    publish solrnews

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,88 @@
+use chrono::{DateTime, Utc};
+use clap::Parser;
+use minidom::{Element, Node};
 extern crate minidom;
 
-const ENTRY_NS: &'static str = "entry";
+const ENTRY_NS: &'static str = "http://www.w3.org/2005/Atom";
+const SOLR_ATOM: &'static str = "https://solr.apache.org/feeds/solr/news.atom.xml";
 
 #[derive(Debug)]
-pub struct News {
-    pub title: String,
-    pub summary: String,
-    pub date: String,
+struct News {
+    title: String,
+    summary: String,
+    date: String,
+}
+
+#[derive(Parser, Debug)]
+#[command(name = "solrnews")]
+#[command(bin_name = "solrnews")]
+#[command(version = "0.0.1")]
+#[command(author, version, about, long_about = None)]
+struct SolrNewsArgs {
+    #[arg(short, long, default_value_t = 5)]
+    take: usize,
 }
 
 fn main() -> Result<(), reqwest::Error> {
-    let mut res = match reqwest::blocking::get("https://solr.apache.org/feeds/solr/news.atom.xml") {
+    let args = SolrNewsArgs::parse();
+
+    let res = match reqwest::blocking::get(SOLR_ATOM) {
         Ok(it) => it,
         Err(err) => return Err(err),
     };
-    let root: minidom::Element = res.text()?.parse().unwrap();
+    let root: Element = res.text()?.parse().unwrap();
 
     root.children()
-        .map(|child| {
-            let title = child.get_child("title", ENTRY_NS).unwrap().text();
-            let date = child.get_child("published", ENTRY_NS).unwrap().text();
-            let summary = child.get_child("summary", ENTRY_NS).unwrap().text();
-            News {
-                title: title,
-                date: date,
-                summary: summary,
+        .filter(|s| s.is("entry", ENTRY_NS))
+        .take(args.take)
+        .map(|entry| {
+            let title = entry
+                .get_child("title", entry.ns().as_str())
+                .unwrap_or(
+                    &Element::builder("title", entry.ns().as_str())
+                        .append(Node::Text("Empty".to_owned()))
+                        .build(),
+                )
+                .text();
+            let date = entry
+                .get_child("published", entry.ns().as_str())
+                .unwrap_or(
+                    &Element::builder("published", entry.ns().as_str())
+                        .append(Node::Text("0000-00-00T00:00:00+00:00".to_owned()))
+                        .build(),
+                )
+                .text()
+                .parse::<DateTime<Utc>>()
+                .unwrap()
+                .to_rfc2822();
+
+            let mut summary: String = entry
+                .get_child("summary", entry.ns().as_str())
+                .map(|c| c.nodes())
+                .unwrap()
+                .filter(|e| e.as_text().is_some())
+                .map(|node| {
+                    return node.as_text().unwrap();
+                })
+                .nth(1)
+                .into_iter()
+                .collect();
+            summary = summary.replace(">", "");
+            return News {
+                title,
+                date,
+                summary,
             };
         })
-        .take(4)
-        .for_each(|news| println!("{:?}", news));
+        .enumerate()
+        .for_each(|item| {
+            println!(
+                "{}. {} - {} - {}",
+                item.0 + 1,
+                item.1.title,
+                item.1.date,
+                item.1.summary
+            );
+        });
     Ok(())
 }
